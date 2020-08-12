1aac53ff37bb1fd24647a05e35bdb2b743283bde
Justin Wood <jwood@me.com>
2020-08-12 11:36:48 -0700

    UPdating

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Requirements
 const mqtt = require('mqtt')
 const _ = require('lodash')
-const repeat = require('repeat')
+const interval = require('interval-promise')
 const logging = require('homeautomation-js-lib/logging.js')
 const Davis = require('davis-weather')
 const mqtt_helpers = require('homeautomation-js-lib/mqtt_helpers.js')
@@ -15,17 +15,17 @@ var weatherstationPort = process.env.DAVIS_PORT
 const client = mqtt_helpers.setupClient(null, null)
 
 if (_.isNil(topic_prefix)) {
-	logging.warn('TOPIC_PREFIX not set, not starting')
-	process.abort()
+    logging.warn('TOPIC_PREFIX not set, not starting')
+    process.abort()
 }
 
 if (_.isNil(weatherstationIP)) {
-	logging.warn('DAVIS_IP not set, not starting')
-	process.abort()
+    logging.warn('DAVIS_IP not set, not starting')
+    process.abort()
 }
 
 if (_.isNil(weatherstationPort)) {
-	weatherstationPort = 22222
+    weatherstationPort = 22222
 }
 
 var mqttOptions = {}
@@ -33,39 +33,41 @@ var mqttOptions = {}
 var shouldRetain = process.env.MQTT_RETAIN
 
 if (_.isNil(shouldRetain)) {
-	shouldRetain = true
+    shouldRetain = true
 }
 
 if (!_.isNil(shouldRetain)) {
-	mqttOptions['retain'] = shouldRetain
+    mqttOptions['retain'] = shouldRetain
 }
 
 var davisClient = new Davis(weatherstationIP, weatherstationPort)
 
 const check_measurements = function() {
-	davisClient.getCurrentConditions(function(err, data) {    
-		if( err ) {
-			logging.info(err)
-		} else {
-			Object.keys(data).forEach(measurement => {
-				const value = data[measurement]
-				if ( !_.isNil(value) ) { 
-					var stringValue = value.toString()
+    davisClient.getCurrentConditions(function(err, data) {
+        if (err) {
+            logging.info(err)
+        } else {
+            Object.keys(data).forEach(measurement => {
+                const value = data[measurement]
+                if (!_.isNil(value)) {
+                    var stringValue = value.toString()
 
-					if ( stringValue != 'NaN' ) { 
-						client.smartPublish(topic_prefix + '/' + measurement, stringValue) 
-					} 
-				} else {
-					logging.debug('missing value for key: ' + measurement)
-				}
-			})
-		}
-	})
+                    if (stringValue != 'NaN') {
+                        client.smartPublish(topic_prefix + '/' + measurement, stringValue)
+                    }
+                } else {
+                    logging.debug('missing value for key: ' + measurement)
+                }
+            })
+        }
+    })
 }
 
 const startMonitoring = function() {
-	logging.info('Starting to monitor: ' + weatherstationIP)
-	repeat(check_measurements).every(30, 's').start.in(1, 'sec')
+    logging.info('Starting to monitor: ' + weatherstationIP)
+    interval(async() => {
+        check_measurements()
+    }, 30 * 1000)
 }
 
 startMonitoring()
