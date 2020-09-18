ffac175528ff7c87c49d9fc8763884a55d62e783
Justin Wood <jwood@me.com>
2020-09-18 09:43:43 -0700

    Updating, and using running average to throw away garbage

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const interval = require('interval-promise')
 const logging = require('homeautomation-js-lib/logging.js')
 const Davis = require('davis-weather')
 const mqtt_helpers = require('homeautomation-js-lib/mqtt_helpers.js')
+const utilities = require('homeautomation-js-lib/utilities.js')
 
 // Config
 const topic_prefix = process.env.TOPIC_PREFIX
@@ -42,6 +43,20 @@ if (!_.isNil(shouldRetain)) {
 
 var davisClient = new Davis(weatherstationIP, weatherstationPort)
 
+
+const VALUES_FOR_RUNNING_AVERAGE = 8
+const MIN_VALUES_FOR_RUNNING_AVERAGE_THRESHOLD = 3
+const THRESHOLD_TO_THROW_AWAY = 20
+const MAX_VALUES_TO_THROW_AWAY = 2
+
+const average_options = {
+    values_for_running_average: VALUES_FOR_RUNNING_AVERAGE,
+    min_values_for_running_average_threshold: MIN_VALUES_FOR_RUNNING_AVERAGE_THRESHOLD,
+    threshold_to_throw_away: THRESHOLD_TO_THROW_AWAY,
+    max_values_to_throw_away: MAX_VALUES_TO_THROW_AWAY
+}
+
+
 const check_measurements = function() {
     davisClient.getCurrentConditions(function(err, data) {
         if (err) {
@@ -51,12 +66,10 @@ const check_measurements = function() {
                 const value = data[measurement]
                 if (!_.isNil(value)) {
                     var stringValue = value.toString()
-
                     if (stringValue != 'NaN') {
-                        client.smartPublish(topic_prefix + '/' + measurement, stringValue)
+                        utilities.add_running_average(measurement, value, average_options)
+                        client.smartPublish(mqtt_helpers.generateTopic(topic_prefix, measurement), utilities.running_average(key))
                     }
-                } else {
-                    logging.debug('missing value for key: ' + measurement)
                 }
             })
         }
@@ -67,7 +80,7 @@ const startMonitoring = function() {
     logging.info('Starting to monitor: ' + weatherstationIP)
     interval(async() => {
         check_measurements()
-    }, 30 * 1000)
+    }, 5 * 1000)
     check_measurements()
 }
 
