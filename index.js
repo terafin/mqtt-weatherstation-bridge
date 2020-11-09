75df9f33e0868d91607b35a1882943d28f55a40d
Justin Wood <jwood@me.com>
2020-11-09 08:35:27 -0800

    Updating to weatherlinkLive

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const mqtt = require('mqtt')
 const _ = require('lodash')
 const interval = require('interval-promise')
 const logging = require('homeautomation-js-lib/logging.js')
-const Davis = require('davis-weather')
+const got = require('got')
 const mqtt_helpers = require('homeautomation-js-lib/mqtt_helpers.js')
 const utilities = require('homeautomation-js-lib/utilities.js')
 
 // Config
 const topic_prefix = process.env.TOPIC_PREFIX
-const weatherstationIP = process.env.DAVIS_IP
-var weatherstationPort = process.env.DAVIS_PORT
+const weatherstationIP = process.env.WEATHERLINK_IP
 
 // Setup MQTT
 const client = mqtt_helpers.setupClient(null, null)
@@ -21,12 +20,8 @@ if (_.isNil(topic_prefix)) {
 }
 
 if (_.isNil(weatherstationIP)) {
-    logging.warn('DAVIS_IP not set, not starting')
+    logging.warn('WEATHERLINK_IP not set, not starting')
     process.abort()
-}
-
-if (_.isNil(weatherstationPort)) {
-    weatherstationPort = 22222
 }
 
 var mqttOptions = {}
@@ -41,7 +36,23 @@ if (!_.isNil(shouldRetain)) {
     mqttOptions['retain'] = shouldRetain
 }
 
-var davisClient = new Davis(weatherstationIP, weatherstationPort)
+async function query_station(station) {
+    const url = 'http://' + station + ':80/v1/current_conditions'
+    logging.info('davis weatherlink request url: ' + url)
+    var error = null
+    var body = null
+    var results = null
+
+    try {
+        const response = await got.get(url)
+        results = JSON.parse(response.body).data
+    } catch (e) {
+        logging.error('failed querying station: ' + e)
+        error = e
+    }
+
+    return results
+}
 
 const average_options = {
     values_for_running_average: 8,
@@ -60,28 +71,19 @@ const slow_moving_average_options = {
 const values_to_use_slow_running_average = ['outsideTemperature', 'outsideHumidity', 'insideHumidity', 'insideTemperature']
 const values_to_use_running_average = ['barometer']
 
-const check_measurements = function() {
-    davisClient.getCurrentConditions(function(err, data) {
-        if (err) {
-            logging.info(err)
-        } else {
-            Object.keys(data).forEach(measurement => {
-                const value = data[measurement]
-                if (isNaN(value)) {
-                    client.smartPublish(mqtt_helpers.generateTopic(topic_prefix, measurement), value)
-                } else if (values_to_use_slow_running_average.includes(measurement)) {
-                    utilities.add_running_average(measurement, value, slow_moving_average_options)
-                    client.smartPublish(mqtt_helpers.generateTopic(topic_prefix, measurement), utilities.running_average(measurement))
-                } else if (values_to_use_running_average.includes(measurement)) {
-                    if (value != 255) { // Occasionally temp and humidity report this
-                        utilities.add_running_average(measurement, value, average_options)
-                    }
-                    client.smartPublish(mqtt_helpers.generateTopic(topic_prefix, measurement), utilities.running_average(measurement))
-                } else {
-                    client.smartPublish(mqtt_helpers.generateTopic(topic_prefix, measurement), value)
-                }
-            })
-        }
+async function check_measurements() {
+    const results = await query_station(weatherstationIP)
+
+    logging.debug('results: ' + JSON.stringify(results))
+
+    const deviceID = results.did
+    const timestamp = results.ts
+
+    client.smartPublish(mqtt_helpers.generateTopic(topic_prefix, 'device_id'), deviceID, mqttOptions)
+    client.smartPublish(mqtt_helpers.generateTopic(topic_prefix, 'timestamp'), timestamp, mqttOptions)
+
+    results.conditions.forEach(condition_set => {
+        client.smartPublishCollection(topic_prefix, condition_set, ['data_structure_type', 'lsid', 'txid'], mqttOptions)
     })
 }
 
@@ -89,7 +91,7 @@ const startMonitoring = function() {
     logging.info('Starting to monitor: ' + weatherstationIP)
     interval(async() => {
         check_measurements()
-    }, 5 * 1000)
+    }, 10 * 1000)
     check_measurements()
 }
 
